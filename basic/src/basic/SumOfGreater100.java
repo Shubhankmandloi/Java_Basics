60d936b680998f2396f64e01d900b9139b745e8d
shubhankmandloi <shubhank2mandloi@gmail.com>
2025-03-21 12:09:35 +0530

    Done

---
+++ basic/src/basic/SumOfGreater100.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class SumOfGreater100 {
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i = 100;i<=200;i++) {
+			if(i%7==0) {
+				sum =sum +i;
+			}
+			System.out.println(sum);	
+		}
+		
+	}
+
+}
